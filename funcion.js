5c232ccf8a4595382c2f0ce308164718f2232bb2
Heaf2023 <126633928+Heaf2023@users.noreply.github.com>
2023-03-01 13:39:13 -0300

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,6 @@
+
+document.getElementById('boton').onclick = function (){
+console.log("capturamos el evento click");
+document.getElementById("Masinfo").innerHTML = " Me apasionan temas como son: La informatica (armado, venta, programacion, reparacion) Internet sus distintos usos, la optimizacion web , los moviles y tecnologias web, etc. Tambien la educacion y su enseñanza."
+
+}
